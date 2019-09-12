aab11b50581f4369e889239ab63c47f0cfcaa1b3
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-09-12 04:02:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,7 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 require "pry"
 
-doc = Nokogiri::HTML(open("http://flatironschool.com"))
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
 doc.css(".site-header__hero__headline")
 puts doc.css(".site-header__hero__headline").text.strip
 binding.pry
